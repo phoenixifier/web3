5490f2ab801478923ba82e39734df4c0eb0c1ea1
Rasul <r.qodiriy@uzinfocom.uz>
2024-10-18 12:22:50 +0500

    fix: ui improvements

--- client/smart-chain/src/components/Loader.tsx
+++ client/smart-chain/src/components/Loader.tsx
@@ -1,7 +1,11 @@
 import type React from "react";
 
 const Loader: React.FC = () => {
-	return <div>sdf</div>;
+	return (
+		<div className="flex justify-center items-center py-3">
+			<div className="animate-spin rounded-full h-24 w-24 border-b-2 border-white" />
+		</div>
+	);
 };
 
 export default Loader;
