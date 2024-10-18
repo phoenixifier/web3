a142c1f4ecf80a610645759ff519ca27b3263e2e
Rasul <r.qodiriy@uzinfocom.uz>
2024-10-18 11:30:49 +0500

    feat: navbar

--- client/smart-chain/src/App.tsx
+++ client/smart-chain/src/App.tsx
@@ -1,7 +1,22 @@
 import type React from "react";
+import Footer from "./components/Footer.tsx";
+import Hero from "./components/Hero.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Services from "./components/Services.tsx";
+import Transactions from "./components/Transactions.tsx";
 
 const App: React.FC = () => {
-	return <div className="bg-amber-400">Hello there</div>;
+	return (
+		<div className="min-h-screen">
+			<div className="gradient-bg-welcome">
+				<Navbar />
+				<Hero />
+			</div>
+			<Services />
+			<Transactions />
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
